430bc0ba623de358fac3fe9e849048c50e6d17c2
realmaacke <mackan.pettersson1@outlook.com>
2022-06-16 17:45:05 +0200

    Initial commit

---
+++ classes/User.php
@@ -0,0 +1,22 @@
+<?php 
+
+class User
+{
+    // Locate User
+    public static function FindUserById($id)
+    {
+        return DB::query('SELECT * From user WHERE id =:id', array(':id'=>$id));
+    }
+
+    public static function FindUserByUserName($UserName)
+    {
+        return DB::query('SELECT * From user WHERE Username =:Username', array(':Username'=>$UserName));
+    }
+
+    // Security Methods
+    public static function AuthenticateUser($username, $password)
+    {
+
+    }
+
+}
